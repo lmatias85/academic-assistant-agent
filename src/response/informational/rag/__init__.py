23f8dea6ed3ec2d43f1c41695690e1fc2da1b727
lmatias85 <35533624+lmatias85@users.noreply.github.com>
2026-01-29 17:23:48 -0300

    README and closure (#23)
    * README.md added
    
    * README fixed
    
    * Final commit for the POC

--- src/response/informational/rag/__init__.py
+++ src/response/informational/rag/__init__.py
@@ -1,7 +1,7 @@
 from src.response.informational.rag.loader import load_rules_document
 from src.response.informational.rag.index import build_faiss_index
 
-_RULES_PATH = "data/Academic_Rules_School_EN.md"
+_RULES_PATH = "data/documentation/Academic_Rules_School_EN.md"
 
 _rules_text = load_rules_document(_RULES_PATH)
 _vectorstore = build_faiss_index(_rules_text)
